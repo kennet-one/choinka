ce68c9a128bf3b83287c68e7ec79c3b4a5c95cd1
kennet-one <kennet1one@gmail.com>
2025-06-10 21:22:05 +0200

    update feedback

--- choinka.ino
+++ choinka.ino
@@ -8,14 +8,28 @@ painlessMesh  mesh;
 const uint8_t    SENSOR_PIN   = A0;
 const int        DRY_VALUE    = 1023;                       
 const int        WET_VALUE    = 0; 
-const int        THRESHOLD   = 15;    // поріг вологості
+const int        THRESHOLD   = 23;    // поріг вологості
 
 void receivedCallback( uint32_t from, String &msg ) {
 
-  if (msg == "tomat0")   digitalWrite(4, LOW);
-  if (msg == "tomat1")   digitalWrite(4, HIGH);
+  if (msg == "tomat0") feedback();
 }
 
+int readsens () {
+  int raw = analogRead(SENSOR_PIN);                      // 0…1023
+  int pct = map(raw, DRY_VALUE, WET_VALUE, 0, 100);      // конвертуємо у % вологості
+  pct = constrain(pct, 0, 100);
+
+  return pct;
+}
+
+void feedback () {
+  int pctt = readsens();
+  String pct = "tomat0=";
+  pct += pctt;
+  mesh.sendSingle(624409705,pct);
+  mesh.sendSingle(1127818912,pct);
+}
 
 Task taskTurnOffRelay(
   0,               
@@ -31,10 +45,7 @@ Task taskReadSensor(
   600000,                    // через 10 мін
   TASK_FOREVER,            // нескінченно
   []() {
-    int raw = analogRead(SENSOR_PIN);                      // 0…1023
-    int pct = map(raw, DRY_VALUE, WET_VALUE, 0, 100);      // конвертуємо у % вологості
-    pct = constrain(pct, 0, 100);
-
+    int pct = readsens();
     Serial.println(pct);
 
     if (pct < THRESHOLD) {
@@ -66,5 +77,4 @@ void setup() {
 
 void loop() {
   mesh.update();
-
 }
