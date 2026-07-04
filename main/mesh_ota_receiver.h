d28b603575e5213864a3333400fa3184dbad4e8e
kennet-one <kennet1one@gmail.com>
2026-07-04 16:12:32 +0200

    use shared mesh runtime

--- main/mesh_ota_receiver.h
+++ main/mesh_ota_receiver.h
@@ -1,19 +1,8 @@
 #pragma once
 
-#include <stdbool.h>
-#include <stddef.h>
+#include "keemash_mesh_ota_receiver.h"
 
-#include "esp_err.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-esp_err_t mesh_ota_receiver_start(void);
-bool mesh_ota_receiver_active(void);
-esp_err_t mesh_ota_receiver_handle_rx(const void *pkt_buf, size_t pkt_len);
-esp_err_t mesh_ota_receiver_handle_v2(const void *payload, size_t payload_len);
-
-#ifdef __cplusplus
-}
-#endif
+#define mesh_ota_receiver_start keemash_mesh_ota_receiver_start
+#define mesh_ota_receiver_active keemash_mesh_ota_receiver_active
+#define mesh_ota_receiver_handle_rx keemash_mesh_ota_receiver_handle_rx
+#define mesh_ota_receiver_handle_v2 keemash_mesh_ota_receiver_handle_v2
